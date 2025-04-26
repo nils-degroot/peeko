e651409ce9c08ecdcaa1fce5954ed2b550f9aca9
nils-degroot <nils@peeko.nl>
2025-04-26 16:13:31 +0200

    Sassing

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,30 @@
 import pugPlugin from "@11ty/eleventy-plugin-pug";
+import path from "node:path";
+import * as sass from "sass";
 
 export default function (eleventyConfig) {
-	eleventyConfig.addPlugin(pugPlugin);
+  eleventyConfig.addPlugin(pugPlugin);
+
+  eleventyConfig.addTemplateFormats("scss");
+
+  eleventyConfig.addExtension("scss", {
+    outputFileExtension: "css",
+    useLayouts: false,
+
+    compile: async function (inputContent, inputPath) {
+      let parsed = path.parse(inputPath);
+
+      if (parsed.name.startsWith("_")) {
+        return;
+      }
+
+      let result = sass.compileString(inputContent, {
+        loadPaths: [parsed.dir || ".", this.config.dir.includes],
+      });
+
+      this.addDependencies(inputPath, result.loadedUrls);
+
+      return async () => result.css;
+    },
+  });
 }
