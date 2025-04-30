b00827005276a954d424fa88c02092a7e246fd41
nils-degroot <nils@peeko.nl>
2025-04-30 11:14:31 +0200

    Purge and mininfy everything

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,24 +1,17 @@
-import pugPlugin from "@11ty/eleventy-plugin-pug";
-import CleanCSS from "clean-css";
 import path from "node:path";
-import * as sass from "sass";
+import { PurgeCSS } from "purgecss";
+import purgeCssFromHtml from "purgecss-from-html";
+import EleventyPluginPug from "@11ty/eleventy-plugin-pug";
+import CleanCSS from "clean-css";
+import { compileString } from "sass";
+import HtmlMinifier from "html-minifier";
 
 export default function (eleventyConfig) {
 	eleventyConfig.setInputDirectory("src");
 
-	eleventyConfig.addPlugin(pugPlugin, {
-		filters: {
-			scssmin: function (text, options) {
-				let result = sass.compileString(text, {
-					loadPaths: ["."],
-				});
-
-				return new CleanCSS({}).minify(result.css).styles;
-			},
-		},
-	});
-
 	eleventyConfig.addTemplateFormats("scss");
+
+	eleventyConfig.addPlugin(EleventyPluginPug);
 
 	eleventyConfig.addExtension("scss", {
 		outputFileExtension: "css",
@@ -31,7 +24,7 @@ export default function (eleventyConfig) {
 				return;
 			}
 
-			let result = sass.compileString(inputContent, {
+			let result = compileString(inputContent, {
 				loadPaths: [parsed.dir || ".", this.config.dir.includes],
 			});
 
@@ -39,6 +32,39 @@ export default function (eleventyConfig) {
 
 			return async () => result.css;
 		},
+	});
+
+	eleventyConfig.addTransform(
+		"purge-and-inline-css",
+		async (content, outputPath) => {
+			if (outputPath && outputPath.endsWith(".html")) {
+				const purgeCssResult = await new PurgeCSS().purge({
+					content: [{ raw: content, extension: "html" }],
+					css: ["_site/main.css"],
+					extractors: [
+						{
+							extractor: purgeCssFromHtml,
+							extensions: ["html"],
+						},
+					],
+				});
+
+				const minifyResult = new CleanCSS().minify(purgeCssResult[0].css);
+
+				return content.replace(
+					"<!-- INLINE CSS -->",
+					`<style>${minifyResult.styles}</style>`,
+				);
+			}
+
+			return content;
+		},
+	);
+
+	eleventyConfig.addTransform("minify-html", async (content, outputPath) => {
+		return outputPath && outputPath.endsWith(".html")
+			? HtmlMinifier.minify(content)
+			: content;
 	});
 
 	eleventyConfig.addPassthroughCopy("src/**/*.js");
